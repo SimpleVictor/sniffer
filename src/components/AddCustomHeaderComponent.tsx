1be412ec3b074f1f42daf4d84aaaa1f1b478bfaa
Viet <me@simplevictor.com>
2018-03-27 22:34:35 -0400

    component: static AddCustomHeader component

---
+++ src/components/AddCustomHeaderComponent.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+class AddCustomHeaderComponent extends Component{
+  constructor(public props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section>
+      </section>
+    )
+  }
+
+}
+
+export default AddCustomHeaderComponent;
