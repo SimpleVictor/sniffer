227834e3d38d67cd9a0278546c8f12f4bf1720a3
Viet <me@simplevictor.com>
2018-03-27 22:35:22 -0400

    component: created a box wrapper component

---
+++ src/components/BoxLoadingComponent.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const BoxLoadingComponent = () => (
+  <div>
+    <div className='boxLoading'/>
+    <span className='box-loading-label__span'>Awaiting requests...</span>
+  </div>
+);
+
+export default BoxLoadingComponent;
+
