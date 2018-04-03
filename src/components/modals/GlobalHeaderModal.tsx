942a4af838b5ee82eb39f256beecd3f12c0a5c3d
Viet <me@simplevictor.com>
2018-04-02 21:55:39 -0400

    chore: create some mock up styling

--- src/components/modals/GlobalHeaderModal.tsx
+++ src/components/modals/GlobalHeaderModal.tsx
@@ -7,14 +7,64 @@ type globalHeaderProperties = {
 }
 
 const GlobalHeaderModal = (props: globalHeaderProperties) => {
+
+  const saveResponse = () => {
+    console.log("khdxsf");
+  }
+
+  const renderList = () => {
+    if(props.globalHeaders && props.globalHeaders.length) {
+      return(
+        <div className="container">
+          {
+            props.globalHeaders.map((header) => (
+              <div className="row sector">
+                <div className="six columns">
+                  <input className="global-header-modal-pre-existing__input" type="text" value={header.headerKey}/>
+                </div>
+                <div className="six columns">
+                  <input className="global-header-modal-pre-existing__input" type="text" value={header.headerValue}/>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      )
+    }else {
+      return (
+        <p>You currently have no global headers set!</p>
+      )
+    }
+  }
+
   return(
     <div id={CSSConstant.globalHeaderModalID} className='ui mini modal'>
-      <div className='header global-header-modal-header__div'>Add Your Global Headers</div>
-        <div className={`content global-header-modal-content__div`}>
-          <div className='ui action input'>
-          {props.globalHeaders.map((e) => (
-            <h3>{e.headerKey} : {e.headerValue}</h3>
-          ))}
+      <div className='header global-header-modal-header__div'>Global Headers</div>
+      <div className={`content global-header-modal-content__div`}>
+        {renderList()}
+        <hr/>
+        <div className="global-header-add-header__container">
+          <label>
+            Key
+            <input className="global-header-modal__input" type="text" name="keyK" />
+          </label>
+          <label>
+            Value
+            <input className="global-header-modal__input" type="text" name="valueK" />
+          </label>
+          <button
+            onClick={() => saveResponse()}
+            className='ui fluid inverted green button'>
+            Add
+          </button>
+        </div>
+        <hr/>
+        <div className="global-header-save__container">
+          <button
+            onClick={() => saveResponse()}
+            className='ui fluid blue button'>
+            Save
+          </button>
         </div>
       </div>
     </div>
