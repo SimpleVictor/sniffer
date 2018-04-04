791e1cd0ce59c12b9ac59ae7c4aaffc737d07369
Viet <me@simplevictor.com>
2018-04-03 21:11:57 -0400

    fix: made the global header modal prettier

--- src/components/modals/GlobalHeaderModal.tsx
+++ src/components/modals/GlobalHeaderModal.tsx
@@ -41,67 +41,87 @@ const GlobalHeaderModal = (props: globalHeaderProperties) => {
     openToast('Saved responses');
   }
 
-  const renderList = () => {
-    if(props.globalHeaders && props.globalHeaders.length) {
-      return(
-        <div className="container">
+  const renderList = props => {
+    if (props.globalHeaders && props.globalHeaders.length) {
+      return (
+        <div>
           {
             props.globalHeaders.map((header, idx) => (
-              <div className="row sector">
-                <div className="five columns">
-                  <input className="global-header-modal-pre-existing__input" type="text" value={header.headerKey}/>
-                </div>
-                <div className="five columns">
-                  <input className="global-header-modal-pre-existing__input" type="text" value={header.headerValue}/>
-                </div>
+              <div className="global-header-list-container" key={header.headerKey+(idx + 1)}>
+                <section className="global-header-list-section global-header-list-section--right">
+                  <input className="global-header-modal-pre-existing__input" type="text" defaultValue={header.headerKey}/>
+                </section>
+
+                <section className="global-header-list-section">
+                  <input className="global-header-modal-pre-existing__input" type="text" defaultValue={header.headerValue}/>
+                </section>
 
                 <div
-                  className="two columns global-header-modal-custom-height"
+                  className="global-header-modal-custom-height"
                   onClick={() => deleteHeader(idx)}>
-                    <i className='fa fa-trash-alt fa-2x global-header-modal-trash__icon'/>
+                  <i className='fas fa-trash-alt fa-2x global-header-modal-trash__icon'/>
                 </div>
+
               </div>
             ))
           }
         </div>
       )
-    }else {
+    } else {
       return (
-        <p>You currently have no global headers set!</p>
+        <section>
+          <i className='fas fa-battery-empty fa-5x global-header-modal-meh__icon'/>
+          <p className='global-header__p'>You Have None...</p>
+        </section>
       )
     }
   }
 
-  return(
+  return (
     <div id={CSSConstant.globalHeaderModalID} className='ui mini modal'>
       <div className='header global-header-modal-header__div'>Global Headers</div>
       <div className={`content global-header-modal-content__div`}>
-        {renderList()}
-        <hr/>
-        <div className="global-header-add-header__container">
-          <label>
-            Key
-            <input id="globalHeaderKeyID" className="global-header-modal__input" type="text" name="keyK" />
-          </label>
-          <label>
-            Value
-            <input id="globalHeaderValueID" className="global-header-modal__input" type="text" name="valueK" />
-          </label>
-          <button
-            onClick={() => addResponses()}
-            className='ui fluid inverted green button'>
-            Add
-          </button>
+
+        <div className='global-header-modal-container'>
+
+          <section className="global-header-add-arrow__container" onClick={() => addResponses()}>
+            <i className="fa fa-chevron-circle-right fa-5x global-header-add-arrow__icon"></i>
+          </section>
+
+          <section className='global-header-modal-flex-section global-header-modal-flex-section--right global-header-modal-add-header-container'>
+
+            <p className='global-header-left-title'>Add Headers</p>
+
+            <input id="globalHeaderKeyID" className="global-header-modal__input" type="text"/>
+            <label className="global-header-modal__label">Key</label>
+
+            <section className='global-header-spacer__section'></section>
+
+            <input id="globalHeaderValueID" className="global-header-modal__input" type="text"/>
+            <label className="global-header-modal__label">Value</label>
+
+          </section>
+
+
+
+          <section className='global-header-modal-flex-section'>
+            <p className='global-header-right-title'>Header List</p>
+            {renderList(props)}
+          </section>
         </div>
-        <hr/>
+
+        <hr className='global-header__hr'/>
+
         <div className="global-header-save__container">
           <button
             onClick={() => saveResponse()}
-            className='ui fluid blue button'>
+            className='ui fluid button global-header-save-button'>
             Save
           </button>
         </div>
+
       </div>
+
     </div>
   )
 }
